5c95652b472348289d211d9a1aef150490c25bf7
fanxq <47813518+xqvan@users.noreply.github.com>
2022-01-31 01:21:35 +0800

    Create gitlab_ubuntu_20.04_init.sh

---
+++ gitlab_ubuntu_20.04_init.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install -y curl openssh-server ca-certificates tzdata perl
+
+
+sudo apt-get install -y postfix
+
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
+
+sudo EXTERNAL_URL="https://gitlab.ggdiag.com" apt-get install gitlab-ee
